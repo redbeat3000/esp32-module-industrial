09345c1a07bf84699033e30926dfe32b7caa1e48
Your Name <you@example.com>
2025-10-08 00:15:51 +0300

    Fix STM32 dependencies and remove rt feature

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
 #![no_std]
 #![no_main]
 
+use defmt::*;
+use defmt_rtt as _;
+use panic_probe as _;
+
 use cortex_m_rt::entry;
-use panic_halt as _;
 
 use stm32f4xx_hal::{
-    gpio::{Output, PushPull},
     pac,
     prelude::*,
 };
@@ -18,21 +20,29 @@ use pins::GpioPins;
 
 #[entry]
 fn main() -> ! {
+    info!("STM32F407 Relay Controller Starting...");
+    
     // Initialize peripherals
     let dp = pac::Peripherals::take().unwrap();
     
-    // Setup clocks
+    // Setup clocks - get RCC from SYSCFG
     let rcc = dp.RCC.constrain();
-    let _clocks = rcc.cfgr.sysclk(84.MHz()).freeze();
+    let clocks = rcc.cfgr.sysclk(84.MHz()).freeze();
     
-    // Initialize GPIO pins
-    let gpioa = dp.GPIOA.split();
-    let gpiob = dp.GPIOB.split();
-    let gpioc = dp.GPIOC.split();
-    let gpiod = dp.GPIOD.split();
+    // Get RCC for GPIO split
+    let mut rcc = unsafe { pac::Peripherals::steal().RCC };
+    
+    // Initialize GPIO pins with RCC reference
+    let gpioa = dp.GPIOA.split(&mut rcc);
+    let gpiob = dp.GPIOB.split(&mut rcc);
+    let gpioc = dp.GPIOC.split(&mut rcc);
+    let gpiod = dp.GPIOD.split(&mut rcc);
     
     let gpio_pins = GpioPins::new(gpioa, gpiob, gpioc, gpiod);
     let mut relay_controller = RelayController::new(gpio_pins);
+    
+    info!("Relay Controller Initialized");
+    info!("Starting main control loop...");
     
     // Main control loop
     let mut counter = 0;
@@ -40,8 +50,12 @@ fn main() -> ! {
         let bank = counter % 4;
         let state = (counter / 4) % 2 == 0;
         
+        info!("Setting bank {} to {}", bank, state);
+        
         let mask = if state { 0xFF } else { 0x00 };
-        let _ = relay_controller.set_bank(bank, mask);
+        if let Err(e) = relay_controller.set_bank(bank, mask) {
+            error!("Error setting bank {}: {}", bank, e);
+        }
         
         counter += 1;
         
--- src/pins.rs
+++ src/pins.rs
@@ -1,17 +1,47 @@
 use stm32f4xx_hal::{
     gpio::{gpioa, gpiob, gpioc, gpiod, Output, PushPull},
-    prelude::*,
 };
 
 pub struct GpioPins {
-    // Bank 0: PA0-PA7
-    pub bank0: [gpioa::PAx<Output<PushPull>>; 8],
-    // Bank 1: PB0-PB7  
-    pub bank1: [gpiob::PBx<Output<PushPull>>; 8],
+    // Bank 0: PA0-PA7 - use individual pin types instead of array
+    pub bank0_pa0: gpioa::PA0<Output<PushPull>>,
+    pub bank0_pa1: gpioa::PA1<Output<PushPull>>,
+    pub bank0_pa2: gpioa::PA2<Output<PushPull>>,
+    pub bank0_pa3: gpioa::PA3<Output<PushPull>>,
+    pub bank0_pa4: gpioa::PA4<Output<PushPull>>,
+    pub bank0_pa5: gpioa::PA5<Output<PushPull>>,
+    pub bank0_pa6: gpioa::PA6<Output<PushPull>>,
+    pub bank0_pa7: gpioa::PA7<Output<PushPull>>,
+    
+    // Bank 1: PB0-PB7
+    pub bank1_pb0: gpiob::PB0<Output<PushPull>>,
+    pub bank1_pb1: gpiob::PB1<Output<PushPull>>,
+    pub bank1_pb2: gpiob::PB2<Output<PushPull>>,
+    pub bank1_pb3: gpiob::PB3<Output<PushPull>>,
+    pub bank1_pb4: gpiob::PB4<Output<PushPull>>,
+    pub bank1_pb5: gpiob::PB5<Output<PushPull>>,
+    pub bank1_pb6: gpiob::PB6<Output<PushPull>>,
+    pub bank1_pb7: gpiob::PB7<Output<PushPull>>,
+    
     // Bank 2: PC0-PC7
-    pub bank2: [gpioc::PCx<Output<PushPull>>; 8],
+    pub bank2_pc0: gpioc::PC0<Output<PushPull>>,
+    pub bank2_pc1: gpioc::PC1<Output<PushPull>>,
+    pub bank2_pc2: gpioc::PC2<Output<PushPull>>,
+    pub bank2_pc3: gpioc::PC3<Output<PushPull>>,
+    pub bank2_pc4: gpioc::PC4<Output<PushPull>>,
+    pub bank2_pc5: gpioc::PC5<Output<PushPull>>,
+    pub bank2_pc6: gpioc::PC6<Output<PushPull>>,
+    pub bank2_pc7: gpioc::PC7<Output<PushPull>>,
+    
     // Bank 3: PD0-PD7
-    pub bank3: [gpiod::PDx<Output<PushPull>>; 8],
+    pub bank3_pd0: gpiod::PD0<Output<PushPull>>,
+    pub bank3_pd1: gpiod::PD1<Output<PushPull>>,
+    pub bank3_pd2: gpiod::PD2<Output<PushPull>>,
+    pub bank3_pd3: gpiod::PD3<Output<PushPull>>,
+    pub bank3_pd4: gpiod::PD4<Output<PushPull>>,
+    pub bank3_pd5: gpiod::PD5<Output<PushPull>>,
+    pub bank3_pd6: gpiod::PD6<Output<PushPull>>,
+    pub bank3_pd7: gpiod::PD7<Output<PushPull>>,
 }
 
 impl GpioPins {
@@ -21,96 +51,110 @@ impl GpioPins {
         gpioc: gpioc::Parts,
         gpiod: gpiod::Parts,
     ) -> Self {
-        // Initialize all 32 pins as outputs
-        let bank0 = [
-            gpioa.pa0.into_push_pull_output(),
-            gpioa.pa1.into_push_pull_output(),
-            gpioa.pa2.into_push_pull_output(),
-            gpioa.pa3.into_push_pull_output(),
-            gpioa.pa4.into_push_pull_output(),
-            gpioa.pa5.into_push_pull_output(),
-            gpioa.pa6.into_push_pull_output(),
-            gpioa.pa7.into_push_pull_output(),
-        ];
-        
-        let bank1 = [
-            gpiob.pb0.into_push_pull_output(),
-            gpiob.pb1.into_push_pull_output(),
-            gpiob.pb2.into_push_pull_output(),
-            gpiob.pb3.into_push_pull_output(),
-            gpiob.pb4.into_push_pull_output(),
-            gpiob.pb5.into_push_pull_output(),
-            gpiob.pb6.into_push_pull_output(),
-            gpiob.pb7.into_push_pull_output(),
-        ];
-        
-        let bank2 = [
-            gpioc.pc0.into_push_pull_output(),
-            gpioc.pc1.into_push_pull_output(),
-            gpioc.pc2.into_push_pull_output(),
-            gpioc.pc3.into_push_pull_output(),
-            gpioc.pc4.into_push_pull_output(),
-            gpioc.pc5.into_push_pull_output(),
-            gpioc.pc6.into_push_pull_output(),
-            gpioc.pc7.into_push_pull_output(),
-        ];
-        
-        let bank3 = [
-            gpiod.pd0.into_push_pull_output(),
-            gpiod.pd1.into_push_pull_output(),
-            gpiod.pd2.into_push_pull_output(),
-            gpiod.pd3.into_push_pull_output(),
-            gpiod.pd4.into_push_pull_output(),
-            gpiod.pd5.into_push_pull_output(),
-            gpiod.pd6.into_push_pull_output(),
-            gpiod.pd7.into_push_pull_output(),
-        ];
-        
         Self {
-            bank0,
-            bank1,
-            bank2,
-            bank3,
+            // Bank 0
+            bank0_pa0: gpioa.pa0.into_push_pull_output(),
+            bank0_pa1: gpioa.pa1.into_push_pull_output(),
+            bank0_pa2: gpioa.pa2.into_push_pull_output(),
+            bank0_pa3: gpioa.pa3.into_push_pull_output(),
+            bank0_pa4: gpioa.pa4.into_push_pull_output(),
+            bank0_pa5: gpioa.pa5.into_push_pull_output(),
+            bank0_pa6: gpioa.pa6.into_push_pull_output(),
+            bank0_pa7: gpioa.pa7.into_push_pull_output(),
+            
+            // Bank 1
+            bank1_pb0: gpiob.pb0.into_push_pull_output(),
+            bank1_pb1: gpiob.pb1.into_push_pull_output(),
+            bank1_pb2: gpiob.pb2.into_push_pull_output(),
+            bank1_pb3: gpiob.pb3.into_push_pull_output(),
+            bank1_pb4: gpiob.pb4.into_push_pull_output(),
+            bank1_pb5: gpiob.pb5.into_push_pull_output(),
+            bank1_pb6: gpiob.pb6.into_push_pull_output(),
+            bank1_pb7: gpiob.pb7.into_push_pull_output(),
+            
+            // Bank 2
+            bank2_pc0: gpioc.pc0.into_push_pull_output(),
+            bank2_pc1: gpioc.pc1.into_push_pull_output(),
+            bank2_pc2: gpioc.pc2.into_push_pull_output(),
+            bank2_pc3: gpioc.pc3.into_push_pull_output(),
+            bank2_pc4: gpioc.pc4.into_push_pull_output(),
+            bank2_pc5: gpioc.pc5.into_push_pull_output(),
+            bank2_pc6: gpioc.pc6.into_push_pull_output(),
+            bank2_pc7: gpioc.pc7.into_push_pull_output(),
+            
+            // Bank 3
+            bank3_pd0: gpiod.pd0.into_push_pull_output(),
+            bank3_pd1: gpiod.pd1.into_push_pull_output(),
+            bank3_pd2: gpiod.pd2.into_push_pull_output(),
+            bank3_pd3: gpiod.pd3.into_push_pull_output(),
+            bank3_pd4: gpiod.pd4.into_push_pull_output(),
+            bank3_pd5: gpiod.pd5.into_push_pull_output(),
+            bank3_pd6: gpiod.pd6.into_push_pull_output(),
+            bank3_pd7: gpiod.pd7.into_push_pull_output(),
         }
     }
     
     pub fn set_bank0_relay(&mut self, relay: u8, state: bool) {
-        if (relay as usize) < self.bank0.len() {
-            if state {
-                let _ = self.bank0[relay as usize].set_high();
-            } else {
-                let _ = self.bank0[relay as usize].set_low();
-            }
+        match relay {
+            0 => self.set_pin_state(&mut self.bank0_pa0, state),
+            1 => self.set_pin_state(&mut self.bank0_pa1, state),
+            2 => self.set_pin_state(&mut self.bank0_pa2, state),
+            3 => self.set_pin_state(&mut self.bank0_pa3, state),
+            4 => self.set_pin_state(&mut self.bank0_pa4, state),
+            5 => self.set_pin_state(&mut self.bank0_pa5, state),
+            6 => self.set_pin_state(&mut self.bank0_pa6, state),
+            7 => self.set_pin_state(&mut self.bank0_pa7, state),
+            _ => (),
         }
     }
     
     pub fn set_bank1_relay(&mut self, relay: u8, state: bool) {
-        if (relay as usize) < self.bank1.len() {
-            if state {
-                let _ = self.bank1[relay as usize].set_high();
-            } else {
-                let _ = self.bank1[relay as usize].set_low();
-            }
+        match relay {
+            0 => self.set_pin_state(&mut self.bank1_pb0, state),
+            1 => self.set_pin_state(&mut self.bank1_pb1, state),
+            2 => self.set_pin_state(&mut self.bank1_pb2, state),
+            3 => self.set_pin_state(&mut self.bank1_pb3, state),
+            4 => self.set_pin_state(&mut self.bank1_pb4, state),
+            5 => self.set_pin_state(&mut self.bank1_pb5, state),
+            6 => self.set_pin_state(&mut self.bank1_pb6, state),
+            7 => self.set_pin_state(&mut self.bank1_pb7, state),
+            _ => (),
         }
     }
     
     pub fn set_bank2_relay(&mut self, relay: u8, state: bool) {
-        if (relay as usize) < self.bank2.len() {
-            if state {
-                let _ = self.bank2[relay as usize].set_high();
-            } else {
-                let _ = self.bank2[relay as usize].set_low();
-            }
+        match relay {
+            0 => self.set_pin_state(&mut self.bank2_pc0, state),
+            1 => self.set_pin_state(&mut self.bank2_pc1, state),
+            2 => self.set_pin_state(&mut self.bank2_pc2, state),
+            3 => self.set_pin_state(&mut self.bank2_pc3, state),
+            4 => self.set_pin_state(&mut self.bank2_pc4, state),
+            5 => self.set_pin_state(&mut self.bank2_pc5, state),
+            6 => self.set_pin_state(&mut self.bank2_pc6, state),
+            7 => self.set_pin_state(&mut self.bank2_pc7, state),
+            _ => (),
         }
     }
     
     pub fn set_bank3_relay(&mut self, relay: u8, state: bool) {
-        if (relay as usize) < self.bank3.len() {
-            if state {
-                let _ = self.bank3[relay as usize].set_high();
-            } else {
-                let _ = self.bank3[relay as usize].set_low();
-            }
+        match relay {
+            0 => self.set_pin_state(&mut self.bank3_pd0, state),
+            1 => self.set_pin_state(&mut self.bank3_pd1, state),
+            2 => self.set_pin_state(&mut self.bank3_pd2, state),
+            3 => self.set_pin_state(&mut self.bank3_pd3, state),
+            4 => self.set_pin_state(&mut self.bank3_pd4, state),
+            5 => self.set_pin_state(&mut self.bank3_pd5, state),
+            6 => self.set_pin_state(&mut self.bank3_pd6, state),
+            7 => self.set_pin_state(&mut self.bank3_pd7, state),
+            _ => (),
+        }
+    }
+    
+    fn set_pin_state<P: embedded_hal::digital::OutputPin>(&mut self, pin: &mut P, state: bool) {
+        if state {
+            let _ = pin.set_high();
+        } else {
+            let _ = pin.set_low();
         }
     }
 }
